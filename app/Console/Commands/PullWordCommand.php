76870b7bbf78e0a870cd49abffc584b4d0106f79
limao <710898853@qq.com>
2016-11-28 10:20:55 +0800

    debug info

--- app/Console/Commands/PullWordCommand.php
+++ app/Console/Commands/PullWordCommand.php
@@ -43,11 +43,12 @@ class PullWordCommand extends Command
      */
     public function handle()
     {
-
+        // dd(scandir(storage_path('/library')));
         foreach (scandir(storage_path('/library')) as $file) {
             $file_path = storage_path('/library') . "/$file";
             if(is_file($file_path)){
                 if(!Library::where('name',$file)->first()){
+                    $this->comment('====== ' . $file);
                     $this->splitText($file_path);
                     Library::create(['name' => $file]);
                 }
